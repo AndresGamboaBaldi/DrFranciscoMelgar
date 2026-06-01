cbfbfc061948dcc14ecf24bc5923037de2c38956
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2026-06-01 18:21:56 -0400

    [Add] Fix bug on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import Home             from './pages/Home'
 import ProfessionalPage from './pages/ProfessionalPage'
 import CalendarRedirect from './pages/CalendarRedirect'
 
+/** Scrolls to top on every route change */
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/"                  element={<Home />} />
         <Route path="/:slug/calendar"    element={<CalendarRedirect />} />
