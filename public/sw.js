f1dc52920a135a9acc4df05bc7eff3e4f09ccfaa
Andres Gamboa Baldi <andresgamboabaldi@gmail.com>
2026-07-20 16:36:28 -0400

    Change logo

--- public/sw.js
+++ public/sw.js
@@ -69,8 +69,8 @@ self.addEventListener('push', (event) => {
   const title = data.title ?? 'Nueva Cita'
   const options = {
     body: data.body ?? '',
-    icon: '/LogoProbo.png',
-    badge: '/LogoProbo.png',
+    icon: '/Probo.png',
+    badge: '/Probo.png',
     tag: 'nueva-cita',
     renotify: true,
     data: { url: data.url ?? '/' },
